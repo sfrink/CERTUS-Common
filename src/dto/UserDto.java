b2ee7d3f82f23a86cfb05ddec6e11ae7d2ff29d2
Steven Frink <sfrink1@gmail.com>
2014-03-24 15:14:56 -0400

    Updated method names in UserDto

--- src/dto/UserDto.java
+++ src/dto/UserDto.java
@@ -17,22 +17,22 @@ public class UserDto implements Serializable {
 	private int administrator_flag;
 	private int status;
 	
-	public int getUser_id() {
+	public int getUserId() {
 		return user_id;
 	}
-	public void setUser_id(int user_id) {
+	public void setUserId(int user_id) {
 		this.user_id = user_id;
 	}
-	public String getFirst_name() {
+	public String getFirstName() {
 		return first_name;
 	}
-	public void setFirst_name(String first_name) {
+	public void setFirstName(String first_name) {
 		this.first_name = first_name;
 	}
-	public String getLast_name() {
+	public String getLastName() {
 		return last_name;
 	}
-	public void setLast_name(String last_name) {
+	public void setLastName(String last_name) {
 		this.last_name = last_name;
 	}
 	public String getEmail() {
@@ -53,34 +53,34 @@ public class UserDto implements Serializable {
 	public void setSalt(String salt) {
 		this.salt = salt;
 	}
-	public String getTemp_password() {
+	public String getTempPassword() {
 		return temp_password;
 	}
-	public void setTemp_password(String temp_password) {
+	public void setTempPassword(String temp_password) {
 		this.temp_password = temp_password;
 	}
-	public String getTemp_salt() {
+	public String getTempSalt() {
 		return temp_salt;
 	}
-	public void setTemp_salt(String temp_salt) {
+	public void setTempSalt(String temp_salt) {
 		this.temp_salt = temp_salt;
 	}
-	public String getActivation_code() {
+	public String getActivationCode() {
 		return activation_code;
 	}
-	public void setActivation_code(String activation_code) {
+	public void setActivationCode(String activation_code) {
 		this.activation_code = activation_code;
 	}
-	public String getPublic_key() {
+	public String getPublicKey() {
 		return public_key;
 	}
-	public void setPublic_key(String public_key) {
+	public void setPublicKey(String public_key) {
 		this.public_key = public_key;
 	}
-	public int getAdministrator_flag() {
+	public int getAdministratorFlag() {
 		return administrator_flag;
 	}
-	public void setAdministrator_flag(int administrator_flag) {
+	public void setAdministratorFlag(int administrator_flag) {
 		this.administrator_flag = administrator_flag;
 	}
 	public int getStatus() {
@@ -96,17 +96,17 @@ public class UserDto implements Serializable {
 		String endOfString = "\n\n";
 
 		out += "User ";
-		out += "id: " + this.getUser_id() + delimiter;
-		out += "first name: " + this.getFirst_name() + delimiter;
-		out += "last name: " + this.getLast_name() + delimiter;
+		out += "id: " + this.getUserId() + delimiter;
+		out += "first name: " + this.getFirstName() + delimiter;
+		out += "last name: " + this.getLastName() + delimiter;
 		out += "email: " + this.getEmail() + delimiter;
 		out += "password: " + this.getPassword() + delimiter;
 		out += "salt: " + this.getSalt() + delimiter;
-		out += "temp_pass: " + this.getTemp_password() + delimiter;
-		out += "temp_salt: " + this.getTemp_salt() + delimiter;
-		out += "activation_code: " + this.getActivation_code() + delimiter;
-		out += "public key: " + this.getPublic_key() + delimiter;
-		out += "admin_flag: " + this.getAdministrator_flag() + delimiter;
+		out += "temp_pass: " + this.getTempPassword() + delimiter;
+		out += "temp_salt: " + this.getTempSalt() + delimiter;
+		out += "activation_code: " + this.getActivationCode() + delimiter;
+		out += "public key: " + this.getPublicKey() + delimiter;
+		out += "admin_flag: " + this.getAdministratorFlag() + delimiter;
 		out += "status: " + this.getStatus() + delimiter;
 
 		out += endOfString;
