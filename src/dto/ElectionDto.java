07328cc0abec6d7a336622d7597fcfa8b8e5d02c
Hirosh <sulochane@yahoo.com>
2014-04-05 22:44:47 -0400

    modified to handle the private election users

--- src/dto/ElectionDto.java
+++ src/dto/ElectionDto.java
@@ -5,6 +5,7 @@ import java.sql.Timestamp;
 import java.util.ArrayList;
 
 import enumeration.ElectionStatus;
+import enumeration.ElectionType;
 
 /**
  * @date : Mar 16, 2014
@@ -18,6 +19,9 @@ public class ElectionDto implements Serializable{
 	private static final int maxLengthElectionDescription = 1024;
 	private static final int maxLengthCandidatesListString = 2048;
 	private static final int maxLengthStartAndCloseDateTime = 128;
+	private static final int maxLengthAllowedVotersEmail = 4096;
+	
+	private static String 	newLine = System.getProperty("line.separator");
 	
 	private static final long serialVersionUID = -5229576755268759820L;
 	
@@ -32,6 +36,12 @@ public class ElectionDto implements Serializable{
 	private String closeDatetime;
 	private ArrayList<CandidateDto> candidateList;
 	private String candidatesListString;
+	private int electionType;
+	private String emailList;
+	private String registeredEmailList;
+	private String unregisteredEmailList;
+	private boolean emailListError = false;
+	private String emailListMessage = "";
 	
 	
 	public int getElectionId() {
@@ -100,6 +110,45 @@ public class ElectionDto implements Serializable{
 	public void setCandidatesListString(String candidateListString) {
 		this.candidatesListString = candidateListString;
 	}
+	public int getElectionType() {
+		return electionType;
+	}
+	public void setElectionType(int electionType) {
+		this.electionType = electionType;
+	}
+	public String getEmailList() {
+		return emailList;
+	}
+	public void setEmailList(String emailList) {
+		this.emailList = emailList;
+	}
+	public String getRegisteredEmailList() {
+		return registeredEmailList;
+	}
+	public void setRegisteredEmailList(String registeredVotersEmailList) {
+		this.registeredEmailList = registeredVotersEmailList;
+	}
+	public void setVotersEmailList(String emailList) {
+		this.registeredEmailList = emailList;
+	}
+	public String getUnregisteredEmailList() {
+		return unregisteredEmailList;
+	}
+	public void setUnregisteredEmailList(String emailList) {
+		this.unregisteredEmailList = emailList;
+	}
+	public boolean isVotersEmailListError() {
+		return emailListError;
+	}
+	public void setVotersEmailListError(boolean emailListError) {
+		this.emailListError = emailListError;
+	}
+	public String getVotersEmailListMessage() {
+		return emailListMessage;
+	}
+	public void setVotersEmailListMessage(String emailListMessage) {
+		this.emailListMessage = emailListMessage;
+	}
 	public static long getSerialversionuid() {
 		return serialVersionUID;
 	}
@@ -132,22 +181,43 @@ public class ElectionDto implements Serializable{
 		valid &= v.isVerified();
 		status += v.getStatus();
 		
+		int[] allowedTypes = {ElectionType.PUBLIC.getCode(), ElectionType.PRIVATE.getCode()};
+		v = iv.validateInt(this.getElectionType(), "Election Type", allowedTypes);
+		valid &= v.isVerified();
+		status += v.getStatus();
+		
+		v = iv.validateStringAllowNull(this.getRegisteredEmailList(), "Allowed users email list", maxLengthAllowedVotersEmail);
+		valid &= v.isVerified();
+		status += v.getStatus();
+		
+		if (this.getEmailList() != null) {
+			String[] voterEmails = this.getEmailList().split(newLine);
+			for (String voterEmail : voterEmails) {
+				Validator vEmail = iv.validateEmail(voterEmail, "Email address [" + voterEmail + "] ");
+				valid &= vEmail.isVerified();
+				status += vEmail.getStatus();
+			}
+		}
+		
+		
 		if (this.getCandidateList() != null) {
 			for (CandidateDto candidate : this.getCandidateList() ) {
 				Validator vCandidate = candidate.Validate();
 				valid &= vCandidate.isVerified();
-				status += "\n" + vCandidate.getStatus();
+				status += vCandidate.getStatus();
 			}
 		}
+		
 		v.setVerified(v.isVerified() && valid);
 		v.setStatus(v.getStatus() + status);
+
 		return v;
 	}
 	
 	public String toString() {
 		String out = "";
-		String delimiter = "\n";
-		String endOfString = "<<< end >>> \n";
+		String delimiter = newLine;
+		String endOfString = "<<< end >>>"+ newLine;
 
 		out += "Election " + delimiter;
 		out += "id\t\t: " + this.getElectionId() + delimiter;
@@ -165,7 +235,10 @@ public class ElectionDto implements Serializable{
 		} else {
 			out += "candidates\t:empty" + delimiter;
 		}
-		
+		out += "Election Type : " + this.getElectionType() + delimiter;
+		out += "Emails String : " + this.getEmailList() + delimiter;
+		out += "Registered Voters Emails String : " + this.getRegisteredEmailList() + delimiter;
+		out += "Unregistered Voters Emails String : " + this.getUnregisteredEmailList() + delimiter;
 		out += endOfString;
 
 		return out;
